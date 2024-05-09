bbb565b170f0a420a30d031c21b24e5183159498
fasdxoox <163231340+fasdxoox@users.noreply.github.com>
2024-05-09 04:58:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -47,6 +47,6 @@ class Config:
         self.ADMINS_ONLY: bool = os.environ.get("ADMINS_ONLY", False)
         self.SPOTIFY_CLIENT_ID: str = os.environ.get("SPOTIFY_CLIENT_ID", None)
         self.SPOTIFY_CLIENT_SECRET: str = os.environ.get("SPOTIFY_CLIENT_SECRET", None)
-
+self.UPSTREAM_REPO: bool = os.environ.get("UPSTREAM_REPO","https://github.com/fasdxoox/MusicPlayer",)
 
 config = Config()
